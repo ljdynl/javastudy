c967b82ee34135869c54d59a9ba88a22496f90e6
ljdynl <malajisi1982@163.com>
2018-02-28 15:43:32 +0800

    test2

--- vuedemo/src/components/layouts.vue
+++ vuedemo/src/components/layouts.vue
@@ -1,13 +1,19 @@
 <template>
   <section class="container" > <!--最外层容器-->
-    <section class="menu"> <!--左边的容器-->
+    <section class="menu">
+      <menus></menus> <!-- * --><!--左边的容器-->
     </section>
     <section class="content-container"><!--右边的容器-->
     </section>
   </section>
 </template>
 <script>
-export default {}
+import menus from './menus'// * 导入刚才我们创建的 menus组件
+export default {
+  components: { // * 注册menus组件，让其可以在template调用
+    menus
+  }
+}
 </script>
 
 <style lang="less">
--- vuedemo/src/router/index.js
+++ vuedemo/src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Layouts from '@/components/layouts'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Layouts',
+      component: Layouts
     }
   ]
 })
